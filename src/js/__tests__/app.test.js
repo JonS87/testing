1cde951e1520d897a8d49c26dcfc509e2be50ed1
Evgeniy Chausov <evgeniy@chausov.ru>
2024-05-31 13:35:43 +0300

    Done app tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,21 +1,17 @@
 import { luhnCheck, paymentSystem } from "../app";
 
 test.each([
-  [{Card: 4716362962767100}, 'true'],
-  [{Card: 5304387593128294}, 'true'],
-  [{Card: 343814508989803}, 'true'],
-  [{Card: 3533348904680033}, 'true'],
-  [{Card: 6011810497325395}, 'true'],
-  [{Card: 30388459663236}, 'true'],
+  [{Card: 4716362962767100}, true],
+  [{Card: 5304387593128294}, true],
+  [{Card: 343814508989803}, true],
+  [{Card: 3533348904680033}, true],
+  [{Card: 6011810497325395}, true],
+  [{Card: 30388459663236}, true],
 ])(
 ('checking the card %s for authenticity'),
-(health, expected) => {
-  const result = luhnCheck(Card);
+(Card, expected) => {
+  const result = luhnCheck(Card.Card);
   expect(result).toBe(expected);
-});
-
-test("should add do something", async () => {
-  await page.goto(baseUrl);
 });
 
 test.each([
@@ -25,10 +21,12 @@ test.each([
   [{Card: 30388459663236}, 'Diners Club'],
   [{Card: 6011810497325395}, 'Discover'],
   [{Card: 3533348904680033}, 'JCB'],
-  [{Card: 2533348904680033}, 'Mir Pay'],
+  [{Card: 2133348904680033}, 'Mir Pay'],
+  [{Card: 5}, 'something else'],
 ])(
 ('checking the card %s for payment system %s'),
-(health, expected) => {
-  const result = paymentSystem(Card);
+(Card, expected) => {
+  const result = paymentSystem(Card.Card);
+  console.log(result);
   expect(result).toBe(expected);
 });
